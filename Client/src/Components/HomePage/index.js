a7b33b24a15b97492c1b5420aa9f0af68abde6af
Gulshan Aggarwal <gulshanaggarwal333@gmail.com>
2022-01-06 17:59:54 +0530

    user readability added

--- Client/src/Components/HomePage/index.js
+++ Client/src/Components/HomePage/index.js
@@ -11,16 +11,15 @@ function Homepage() {
       <main className="py-16">
         <div className="text-center py-12 space-y-1">
           <h2 className="text-2xl font-medium sm:text-4xl">Looking for a Form Solution ? </h2>
-          <p className="pt-4">Then you are on the right place,</p>
-          <p>No need to write server side programming anymore,</p>
-          <p>Get your keys and integrate into your front end code</p>
+          <p className="py-4 text-xl">Then you are on the right place,</p>
+          <p className="text-xl">No need to write server side programming anymore 😎</p>
         </div>
 
         <div className="bg-gray-900 text-gray-50 w-5/6 py-4 px-4 mx-auto sm:py-8 sm:px-8 rounded-md sm:w-1/2">
           <div className="w-5/6 mx-auto flex flex-col">
-            <span>&lt;form action="https://devformms.herokuapp.com/myapikey" method="post"&gt;</span>
-            <span className="px-4">&lt;label for="email"&gt;Your Email&lt;/label&gt;</span>
-            <span className="px-4">&lt;input name="Email" id="email" type="email" /&gt;</span>
+            <span>&lt;form action=<span className="text-green-300 font-medium">"https://devformms.herokuapp.com/endpoint"</span> method="post"&gt;</span>
+            <span className="px-4">&lt;label for="name"&gt;Your Name&lt;/label&gt;</span>
+            <span className="px-4">&lt;input name="name" id="name" type="text" /&gt;</span>
             <span className="px-4">&lt;button type="submit"&gt;Submit&lt;/button&gt;</span>
             <span>&lt;/form&gt;</span>
           </div>
--- Client/src/Components/Integration/Intergration.js
+++ Client/src/Components/Integration/Intergration.js
@@ -23,8 +23,8 @@ export default function Integration() {
   };
 
   return (
-    <section className="px-16 py-8 bg-blue-300">
-      <div>
+    <section>
+      <div className="px-16 py-8 bg-blue-300">
         <h3 className="font-medium text-xl">Your Form's endpoint is :</h3>
         <div className="flex py-4 space-x-6">
           <div className="pl-4 pr-28 py-3 bg-gray-300 rounded-md url-box">
@@ -38,6 +38,33 @@ export default function Integration() {
             Copy
           </button>
         </div>
+      </div>
+      <div className="px-16 py-8 w-3/4">
+        <h3 className="text-xl font-medium py-8">Intergration Guide</h3>
+        <div className="bg-gray-900 text-gray-50  py-8 text-sm  rounded-md">
+          <div className="w-5/6 mx-auto flex flex-col">
+            <span>&lt;form action=<span className="text-green-300 font-medium">{`"${FormURL}"`}</span> method="post"&gt;</span>
+            <span className="px-4">&lt;label for="name"&gt;Your Name&lt;/label&gt;</span>
+            <span className="px-4">&lt;input name="name" id="name" type="text" /&gt;</span>
+            <span className="px-4">&lt;button type="submit"&gt;Submit&lt;/button&gt;</span>
+            <span>&lt;/form&gt;</span>
+          </div>
+        </div>
+        <div className="py-4 space-y-3">
+          <div>
+            <span className="text-xl font-medium text-blue-500">Step 1:</span>
+            <span className="px-4">You need to copy Form's endpoint from the endpoint section.</span>
+          </div>
+          <div>
+            <span className="text-xl font-medium text-blue-500">Step 2:</span>
+            <span className="px-4">Paste the endpoint into front end code like mentioned above.</span>
+          </div>
+          <div>
+            <span className="text-xl font-medium text-blue-500">Step 3:</span>
+            <span className="px-4">Relax, when someone submits the form on your website we will handle automatically.</span>
+          </div>
+        </div>
+        <p className="py-4 tetx-xl"><span className="text-red-500 font-medium">Note:</span> You can set form fields according to your requirements, not limited to demo form. </p>
       </div>
     </section>
   );
